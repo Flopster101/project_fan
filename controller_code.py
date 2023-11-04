1dde9059cc5444dbd935a0c3caed378632100029
Joel Gómez <nahuelgomez329@gmail.com>
2023-11-04 04:13:03 -0300

    Optimization, error handling, etc...
    Also, provide a precompiled version.
    
    Signed-off-by: Joel Gómez <nahuelgomez329@gmail.com>

--- controller_code.py
+++ controller_code.py
@@ -18,7 +18,10 @@ from adafruit_ds18x20 import DS18X20
 ow_bus = OneWireBus(board.GP22)
 
 # Scan for sensors and grab the first one found.
-ds18b20 = DS18X20(ow_bus, ow_bus.scan()[0])
+try:
+    ds18b20 = DS18X20(ow_bus, ow_bus.scan()[0])
+except IndexError:
+    print("ERROR: Could not initialize temp sensor")
 
 # Define pins
 relay_pins = [board.GP18, board.GP17, board.GP16]
@@ -141,7 +144,6 @@ def set_speed(value):
     update_relays()
     update_display()
     beep()
-    save_settings()
 
 # Function to increment speed relays
 def increment_speed():
@@ -149,9 +151,8 @@ def increment_speed():
     if current_speed < 3:
         current_speed += 1
         update_relays()
-        update_display()
+        update_display_speed()
         beep()
-        save_settings()
         return True
     return False
 
@@ -161,18 +162,15 @@ def decrement_speed():
     if current_speed > 1:
         current_speed -= 1
         update_relays()
-        update_display()
+        update_display_speed()
         beep()
-        save_settings()
         return True
     return False
 
 # Function to update they status of the relays after a change
 def update_relays():
     if power_state:
-        aux_relay.value = True # Turn on aux relay briefly.
-        time.sleep(0.05) # Wait for 50ms.
-        aux_relay.value = False # Turn off aux relay.
+        aux_relay.value = False # Turn on aux relay.
         
         for i in range(3):
             relays[i].value = i != current_speed - 1 # Set relays according to current speed.
@@ -181,31 +179,6 @@ def update_relays():
         
         for i in range(3):
             relays[i].value = True # Turn off all speed relays.
-
-def update_display():
-    # Clear the display.
-    display.fill(0)
-    
-    # Display messages on the left half of the screen.
-    display.text("Speed:" + speed_messages[current_speed - 1], 0, 0, 1)
-    display.text("Tcon:" + ("ON" if temp_control else "OFF"), 0, 10, 1)
-    
-    if panic_state:
-        display.text("Temp:" + str(current_temp) + "C !", 0, 20, 1)
-    else:
-        display.text("Temp:" + str(current_temp) + "C", 0, 20, 1)
-    
-    # Draw a dividing line.
-    display.line(64, 0, 64, display.height - 1, 1)
-    
-    # Display message on the right half of the screen.
-    display.text("Power:" + ("ON" if power_state else "OFF"), 68, 0, 1)
-    
-    # Display beep status on the right half of the screen.
-    display.text("Beep:" + ("ON" if beep_en else "OFF"), 68, 10, 1)
-    
-    # Update the display.
-    display.show()
 
 def config_init():
     global current_speed, temp_control, temp_safe_threshold, power_state, beep_en
@@ -227,7 +200,11 @@ def load_settings():
     global current_speed, temp_control, temp_safe_threshold, power_state, beep_en
     
     with open('settings.json', 'r') as f:
-        settings_data = json.load(f)
+        try:
+            settings_data = json.load(f)
+        except ValueError:
+            config_init()
+            init_controller()
         
         current_speed = settings_data["current_speed"]
         temp_control = settings_data["temp_control"]
@@ -244,8 +221,12 @@ def save_settings():
         "beep_en": beep_en,
     }
     
-    with open('settings.json', 'w') as f:
-        json.dump(settings_data, f)
+    try:
+        with open('settings.json', 'w') as f:
+            json.dump(settings_data, f)
+        print("INFO: settings saved")
+    except OSError:
+        print("ERROR: Saving failed! Filesystem read-only or corrupted file.")
 
 def panic_temp():
     global power_state
@@ -256,19 +237,19 @@ def panic_temp():
 async def update_temp():
     global current_temp
     global panic_state
-    current_temp = int(ds18b20.temperature)
+    try:
+        current_temp = int(ds18b20.temperature)
+    except NameError:
+        print("ERROR: failed to read from temp sensor")
+        current_temp = 0
+    except RuntimeError:
+        current_temp = 0
+        print("ERROR: failed to read from temp sensor")
     
     if init_done:
-        # Clear the line that shows the temperature value.
-        display.fill_rect(0, 20, 50, 8, 0)
-        
-        if panic_state:
-            display.text("Temp:" + str(current_temp) + "C !", 0, 20, 1)
-        else:
-            display.text("Temp:" + str(current_temp) + "C", 0, 20, 1)
-        
-        # Update the display.
-        display.show()
+        update_display_temp()
+    
+    print("INFO: temp updated")
     
     if current_temp > temp_safe_threshold:
         panic_state = True
@@ -276,48 +257,121 @@ async def update_temp():
         if temp_control:
             panic_temp()
 
+## Display update code
+def update_display():
+    # Clear the display.
+    display.fill(0)
+    
+    # Display messages on the left half of the screen.
+    display.text("Speed:" + speed_messages[current_speed - 1], 0, 0, 1)
+    display.text("Tcon:" + ("ON" if temp_control else "OFF"), 0, 12, 1)
+    
+    if panic_state:
+        display.text("Temp:" + str(current_temp) + "C !", 0, 24, 1)
+    else:
+        display.text("Temp:" + str(current_temp) + "C", 0, 24, 1)
+    
+    # Draw a dividing line.
+    display.line(64, 0, 64, display.height - 1, 1)
+    
+    # Display message on the right half of the screen.
+    display.text("Power:" + ("ON" if power_state else "OFF"), 68, 0, 1)
+    
+    # Display beep status on the right half of the screen.
+    display.text("Beep:" + ("ON" if beep_en else "OFF"), 68, 12, 1)
+    
+    # Update the display.
+    display.show()
+    
+def update_display_temp():
+    # Clear the line that shows the temperature value.
+    display.fill_rect(0, 24, 50, 8, 0)
+    
+    if panic_state:
+        display.text("Temp:" + str(current_temp) + "C !", 0, 24, 1)
+    else:
+        display.text("Temp:" + str(current_temp) + "C", 0, 24, 1)
+    
+    # Update the display.
+    display.show()
+            
+def update_display_speed():
+    # Clear the line that shows the speed value.
+    display.fill_rect(0, 0, 60, 8, 0)
+    
+    display.text("Speed:" + speed_messages[current_speed - 1], 0, 0, 1)
+    
+    # Update the display.
+    display.show()
+        
+def update_display_tcon():
+    # Clear the line that shows the TCON state.
+    display.fill_rect(0, 12, 60, 8, 0)
+    
+    display.text("Tcon:" + ("ON" if temp_control else "OFF"), 0, 12, 1)
+    
+    # Update the display.
+    display.show()
+    
+def update_display_power():
+    # Clear the line that shows the power state.
+    display.fill_rect(68, 0, 60, 8, 0)
+    
+    display.text("Power:" + ("ON" if power_state else "OFF"), 68, 0, 1)
+    
+    # Update the display.
+    display.show()
+    
+def update_display_beep():
+    # Clear the line that shows the beeper state.
+    display.fill_rect(68, 12, 60, 8, 0)
+    
+    display.text("Beep:" + ("ON" if beep_en else "OFF"), 68, 12, 1)
+    
+    # Update the display.
+    display.show()
+
 # Credits easter egg
 def credits():
     display.fill(0)
-    display.text("Fan Control Module v1.0", 0, 0, 1)
+    display.text("Fan Control Module v1", 0, 0, 1)
     display.text("Nahuel Gomez", 0, 10, 1)
     display.text("2023 - Flopster101", 0, 20, 1)
     display.show()
     time.sleep(3)
     update_display()
 
+## Toggles
 # Enable/disable beeper
 def beep_toggle():
     global beep_en
     beep_en = not beep_en
-    print(f"beep status set to {beep_en}")
-    update_display()
-    save_settings()
+    print(f"INFO: beep status set to {beep_en}")
+    beep()
+    update_display_beep()
     
 # Toggle power state
 def power_toggle():
     global power_state
     power_state = not power_state
     beep()
-    print(f"power status set to {power_state}")
-    update_display()
+    print(f"INFO: power status set to {power_state}")
+    update_display_power()
     update_relays()
-    save_settings()
 
 # Toggle temperature control
 def tcon_toggle():
     global temp_control
     temp_control = not temp_control
     beep()
-    print(f"temp control status set to {temp_control}")
-    update_display()
-    save_settings()
+    print(f"INFO: temp control status set to {temp_control}")
+    update_display_tcon()
     
 # Reset device and configuration
 def reset():
     global init_done
     global first_boot
-    print("Resetting...")
+    print("INFO: Resetting...")
     display.fill(0)
     display.text("Resetting...", 0, 0, 1)
     display.show()
@@ -346,13 +400,11 @@ def init_controller():
         display.text("Initializing...", 0, 0, 1)
         display.show()
         
-        time.sleep(0.3)
-        
         update_display()
         update_relays()
         
         init_done = True
-        print("Controller initialized")
+        print("INFO: Controller initialized")
 
         # Start main loop
         main()
@@ -362,10 +414,12 @@ def main():
     button3_press_time = 0
     button2_press_time = 0
     last_temp_update = time.monotonic()
+    last_save = time.monotonic()
+    last_num_pulses = len(pulsein)
 
     while True:
         current_time = time.monotonic()
-        last_num_pulses = len(pulsein)
+        num_pulses = len(pulsein)
         
         ## Speed control buttons
         # Update the state of the speed change buttons
@@ -377,18 +431,18 @@ def main():
         ## Button 0 (speed decrease)
         if debouncer_0.fell:
             decrement_speed()
-            print("gp6 press")
+            print("INFO: gp6 press")
             time.sleep(0.05)
             
         ## Button 1  (speed increase)
         if debouncer_1.fell:
             increment_speed()
-            print("gp7 press")
+            print("INFO: gp7 press")
             time.sleep(0.05)
             
         ## Button 2 (reset/tcon toggle)
         if not button_2.value:  # Button is pressed
-            print("gp8 press")
+            print("INFO: gp8 press")
             if button2_press_time == 0:
                 button2_press_time = time.monotonic()
             elif time.monotonic() - button2_press_time > 1:  # Button held for > 1 second
@@ -404,7 +458,7 @@ def main():
             
         ## Button 3 (power/beep toggle)
         if not button_3.value:  # Button is pressed
-            print("gp9 press")
+            print("INFO: gp9 press")
             if button3_press_time == 0:
                 button3_press_time = time.monotonic()
             elif time.monotonic() - button3_press_time > 1:  # Button held for > 1 second
@@ -418,10 +472,8 @@ def main():
                 power_toggle()
                 button3_press_time = 0
         
-        num_pulses = len(pulsein)
-        if num_pulses != last_num_pulses:  # Check if there are new pulses
-            print("pulse received")
-            last_num_pulses = num_pulses
+        if num_pulses > last_num_pulses:  # Check if there are new pulses
+            print("INFO: pulse received")
             # Check for received codes.
             hex_code = receive_decode_ir()
         
@@ -430,12 +482,18 @@ def main():
                     if hex_code == value["code"]:
                         print(f"Key pressed: {key}")
                         value["function"]()  # Call the associated function
+                        
+            last_num_pulses = 0
 
-        # Update temperature every second.
+        # Update temperature every 5 seconds.
         if current_time - last_temp_update > 5:
-            print("temp updated")
             asyncio.run(update_temp())
             last_temp_update = current_time
+        
+        # Save settings only every 6 seconds.
+        if current_time - last_save > 6:
+            save_settings()
+            last_save = current_time
 
 # Check for the existence of settings.json.
 try:
